0a91b02f501db4f44d31b6f60bc6509c8d583b56
Rodrigo Morán <rodrigobq18@gmail.com>
2024-11-16 12:55:01 -0300

    Initials Challenge

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("RRRR   M   M");
+        System.out.println("R   R  MM MM");
+        System.out.println("R   R  M M M");
+        System.out.println("RRRR   M   M");
+        System.out.println("R R    M   M");
+        System.out.println("R  R   M   M");
+        System.out.println("R   R  M   M");
     }
+
 }
